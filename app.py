348eb41e97cbdef9bd1b446a3be167230246f8a7
SIRSTN <stephane.velen@gmail.com>
2024-03-14 09:58:44 +0100

    cloud commit

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def update_deal(db, deal_uid, version_seq, new_inactive_flag):
         {"$set": {"InactiveFlag": new_inactive_flag}}
     )
 
-def create_deal(db, deal_uid, deal_keyword, bav_date, bav_volume, bav_price, bav_amount, version_seq):
+def create_deal(db, deal_uid, deal_keyword, bav_date, bav_volume, bav_price, bav_amount, factor, version_seq):
     deals_collection = db["Deals"]
     new_deal = {
         "DealUID": deal_uid,
@@ -35,20 +35,11 @@ def create_deal(db, deal_uid, deal_keyword, bav_date, bav_volume, bav_price, bav
         "Volume": bav_volume,
         "Price": bav_price,
         "Amount": bav_amount,
+        "Factor": factor,
         "VersionSEQ": version_seq,
         "InactiveFlag": "N"
     }
     deals_collection.insert_one(new_deal)
-
-def get_last_transaction(db, deal_uid, type):
-    transactions_collection = db["Transactions"]
-    last_sell_transaction = transactions_collection.find_one(
-        {"DealUID": deal_uid, "Type": type, "InactiveFlag": "N"},
-        sort=[("Date", pymongo.DESCENDING)]
-    )
-    return last_sell_transaction
-
-import pymongo
 
 def get_sell_transactions(db, deal_uid):
     transactions_collection = db["Transactions"]
@@ -62,18 +53,13 @@ def get_sell_transactions(db, deal_uid):
         sold_amount += transaction.get("Amount", 0)
     return sold_volume, sold_amount
 
-def update_last_transaction(db, deal_uid, type):
-    last_sell_transaction = get_last_transaction(db, deal_uid, type)
-    if last_sell_transaction:
-        transactions_collection = db["Transactions"]
-        transaction_id = last_sell_transaction.get("_id")
-        transactions_collection.update_one(
-            {"_id": transaction_id},
-            {"$set": {"InactiveFlag": "Y"}}
-        )
-        return True
-    else:
-        return False
+def update_sell_transactions(db, deal_uid):
+    transactions_collection = db["Transactions"]
+    result = transactions_collection.update_many(
+        {"DealUID": deal_uid, "Type": "Sell", "InactiveFlag": "N"},
+        {"$set": {"InactiveFlag": "Y"}}
+    )
+    return True
 
 def create_transaction(db, deal_uid, transaction_type, date, volume, price, amount):
     transactions_collection = db["Transactions"]
@@ -89,10 +75,11 @@ def create_transaction(db, deal_uid, transaction_type, date, volume, price, amou
     transactions_collection.insert_one(transaction)
 
 def get_last_valuation(db, deal_uid):
+    from pymongo import DESCENDING
     valuations_collection = db["Valuations"]
     latest_valuation = valuations_collection.find_one(
         {"DealUID": deal_uid},
-        sort=[("Date", pymongo.DESCENDING)]
+        sort=[("Date", DESCENDING)]
     )
     return latest_valuation
 
@@ -129,7 +116,9 @@ def valuate_deals():
             deal_uid = deal.get("DealUID")
             deal_versionSEQ = deal.get("VersionSEQ")
             deal_Volume = deal.get("Volume") 
-            deal_Amount = deal.get("Amount") 
+            deal_Amount = deal.get("Amount")
+            deal_factor = deal.get("Factor")
+            factor = 1 + float(deal_factor)
  
             last_valuation = get_last_valuation(db, deal_uid)
             init_volume = last_valuation.get("Init_Volume")
@@ -138,33 +127,35 @@ def valuate_deals():
                 
                 bav_volume = deal_Volume
                 bav_amount = bav_volume * bav_price
-                if bav_amount >= deal_Amount * 1.05:
+                sold_volume, sold_amount = get_sell_transactions(db, deal_uid)
+
+                if bav_amount >= deal_Amount * factor:
                     sell_amount = bav_amount - deal_Amount
                     sell_volume = sell_amount / bav_price
                     bav_volume = bav_volume - sell_volume
                     bav_amount = bav_volume * bav_price
                     deal_price = deal_Amount / bav_volume
+                    sold_volume = sold_volume + sell_volume
+                    sold_amount = sold_amount + sell_amount
                     create_transaction(db, deal_uid, "Sell", bav_date, sell_volume, bav_price, sell_amount)
                     update_deal(db, deal_uid, deal_versionSEQ, "Y")
-                    create_deal(db, deal_uid, keyword, bav_date, bav_volume, deal_price, deal_Amount, deal_versionSEQ + 1)
+                    create_deal(db, deal_uid, keyword, bav_date, bav_volume, deal_price, deal_Amount, deal_factor, deal_versionSEQ + 1)
                 else:
-                    last_sell_transaction = get_last_transaction(db, deal_uid, "Sell")
-                    if last_sell_transaction:
-                        sell_volume = last_sell_transaction.get("Volume")
-                        sell_amount = last_sell_transaction.get("Amount")
-                        if (deal_Amount - bav_amount) > sell_amount:
-                            buy_amount = sell_amount
+                    if bav_price <= init_price: 
+                        if sold_amount > 0:
+                            buy_amount = sold_amount
                             buy_volume = buy_amount / bav_price
                             bav_volume = bav_volume + buy_volume
                             bav_amount = bav_volume * bav_price
-                            deal_Amount = ((buy_volume - sell_volume) * init_price) + deal_Amount
+                            deal_Amount = ((buy_volume - sold_volume) * init_price) + deal_Amount
                             deal_price = deal_Amount / bav_volume
-                            update_last_transaction(db, deal_uid, "Sell")
+                            sold_volume = 0
+                            sold_amount = 0
+                            update_sell_transactions(db, deal_uid)
                             create_transaction(db, deal_uid, "Buy", bav_date, buy_volume, bav_price, buy_amount)
                             update_deal(db, deal_uid, deal_versionSEQ, "Y")
-                            create_deal(db, deal_uid, keyword, bav_date, bav_volume, deal_price, deal_Amount, deal_versionSEQ + 1)
+                            create_deal(db, deal_uid, keyword, bav_date, bav_volume, deal_price, deal_Amount, deal_factor, deal_versionSEQ + 1)
                 
-                sold_volume, sold_amount = get_sell_transactions(db, deal_uid)
                 create_valuation(db, deal_uid, keyword, bav_date, bav_volume, bav_price, bav_amount, init_volume, init_price, sold_volume, sold_amount)
         
         return jsonify({"message": "Updated documents and inserted transactions successfully"}), 200
